27418daf429f627b803acc5f717c0c6716189cd0
Nenavath Naresh <nenavathnaresh460@gmail.com>
2024-03-27 13:09:09 +0530

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,45 +3,19 @@ import { BrowserModule, bootstrapApplication, provideClientHydration } from '@an
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormComponent } from './form/form.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
-import { CountriesComponent } from './countries/countries.component';
-import { CountryDetailsComponent } from './country-details/country-details.component';
+
 import { HttpClientModule } from '@angular/common/http';
-import { ProductsComponent } from './products/products.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { ProfileComponent } from './profile/profile.component';
-import { EducationComponent } from './education/education.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { AchivementsComponent } from './achivements/achivements.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent,
-    DynamicFormComponent,
-    CountriesComponent,
-    CountryDetailsComponent,
-    ProductsComponent,
-    ProductDetailsComponent,
-    HomeComponent,
-    LoginComponent,
-    RegistrationComponent,
-    ProfileComponent,
-    EducationComponent,
-    ProjectsComponent,
-    AchivementsComponent,
+    
    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
   ],
   providers: [
